2e9f2f3517a76fd194dfa93e644af2843278c6b8
Jan Hatapka <jan.hatapka@gmail.com>
2023-02-04 16:09:25 +0100

    requirements

--- shop/__init__.py
+++ shop/__init__.py
@@ -4,8 +4,8 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:komojini@localhost:5432/johnnys_shop'
-app.config['SECRET_KEY'] = '7cbebf954748b1a7f55c'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:example@localhost:5432/johnnys_shop'
+app.config['SECRET_KEY'] = 'example'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
